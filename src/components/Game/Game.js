84d1871e89c13aa90ca09bf0674ce65ad96abde9
Alvee Akand <alvee.akand@outlook.com>
2021-05-24 02:41:31 -0400

    added states to Game component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,7 +2,23 @@ import React, { useState, useEffect } from 'react'
 import { generateIntegerCode } from '../../api/codeGenerator'
 import Board from '../Board/Board'
 
-function Game() {  
+const defaultCode = [-1, -1, -1, -1];
+const defaultOptions = [0, 1, 2, 3, 4, 5, 6, 7];
+const defaultGameState = {
+  isPlaying: true,
+  code: defaultCode,
+  turn: 0,
+  rows: [],
+  currentOption: 0,
+  currentRow: 9,
+  options: defaultOptions,
+  isWon: false
+}
+
+function Game() {
+  const [code, setCode] = useState(defaultCode);
+  const [gameState, setGameState] = useState(defaultGameState);
+
   return (
     <div className="game">
       <div className="game-board">
