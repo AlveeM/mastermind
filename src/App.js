0dd6569d253ecc814aa1b624c616269db69c4685
Alvee Akand <alvee.akand@outlook.com>
2021-05-24 01:45:46 -0400

    added Game component skeleton

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
-
-import { generateIntegerCode } from './api/codeGenerator'
+import Game from './components/Game/Game';
 
 function App() {
-  useEffect(() => {
-    generateIntegerCode().then(console.log)
-  }, []) 
-
   return (
     <div className="App">
-      <h1>Mastermind</h1>
+      <Game />
     </div>
   );
 }
---
+++ src/components/Game/Game.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import { generateIntegerCode } from '../../api/codeGenerator'
+
+function Game() {  
+  return (
+    <div className="game">
+    
+    </div>
+  )
+}
+
+export default Game;
