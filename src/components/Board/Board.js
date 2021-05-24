ee62a31f7b5f384f4bef64a7fd0d4dc894fdc3a9
Alvee Akand <alvee.akand@outlook.com>
2021-05-24 02:05:09 -0400

    added Board component skeleton

---
+++ src/components/Board/Board.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Board() {
+  return (
+    <table className="board">
+      <tbody>
+        
+      </tbody>
+    </table>
+  )
+}
+
+export default Board;
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { generateIntegerCode } from '../../api/codeGenerator'
+import Board from '../Board/Board'
 
 function Game() {  
   return (
     <div className="game">
-    
+      <div className="game-board">
+        <Board />
+      </div>
     </div>
   )
 }
