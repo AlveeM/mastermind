23c77dc0a6f6af1a7b2791dbb8a5dde485d3049c
Alvee Akand <alvee.akand@outlook.com>
2021-05-24 03:20:20 -0400

    updated default Game state

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,11 +4,12 @@ import Board from '../Board/Board'
 
 const defaultCode = [-1, -1, -1, -1];
 const defaultOptions = [0, 1, 2, 3, 4, 5, 6, 7];
+const defaultRows = getDefaultRows();
 const defaultGameState = {
   isPlaying: true,
   code: defaultCode,
   turn: 0,
-  rows: [],
+  rows: defaultRows,
   currentOption: 0,
   currentRow: 9,
   options: defaultOptions,
@@ -22,10 +23,21 @@ function Game() {
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board gameState={gameState} />
       </div>
     </div>
   )
 }
 
 export default Game;
+
+function getDefaultRows() {
+  const rows = [];
+  for (let i = 0; i <= 9; i++) {
+    rows.push({
+      clues: [-1, -1, -1, -1],
+      inputs: [-1, -1, -1, -1]
+    })
+  }
+  return rows;
+}
