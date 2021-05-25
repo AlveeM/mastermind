a17fe592e0aeea34d90799560ec90e36a28b0bee
Alvee Akand <alvee.akand@outlook.com>
2021-05-24 21:01:14 -0400

    fixed Slot and Slots selection bug

--- src/components/Slot/Slot.js
+++ src/components/Slot/Slot.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Slot.css"
 
-function Slot() {
-  const slotClass = `slot slot--unselected slot--active`;
+function Slot({ row, col, value, currentRow, updateSlot}) {
+  let slotClass = `slot slot--active`;
+
+  if (value > -1) {
+    slotClass += ' slot--selected';
+  }
+
+  function selectSlot() {
+    if (row >= 0) {
+      updateSlot(row, col);
+    }
+  }
 
   return (
-    <div className={slotClass}>0</div>
+    <div className={slotClass} onClick={selectSlot}>
+      {value >= 0 ? value : undefined}
+    </div>
   )
 }
 
--- src/components/Slots/Slots.js
+++ src/components/Slots/Slots.js
@@ -2,13 +2,19 @@ import React from 'react'
 import Slot from '../Slot/Slot'
 import "./Slots.css"
 
-function Slots() {
+function Slots({ slots, row, currentRow, updateSlot }) {
   return (
     <div className="slots">
-      <Slot />
-      <Slot />
-      <Slot />
-      <Slot />
+      {slots.map((value, col) => {
+        return <Slot {...{
+                key: `slot-${row}-${col}`,
+                value,
+                row,
+                col,
+                currentRow,
+                updateSlot }} 
+              />
+      })}
     </div>
   )
 }
